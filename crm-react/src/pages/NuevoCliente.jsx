1a2e5fa41b7cc0b28a8d1471036d94962704f709
Gustavo Marin <wstov@hotmail.com>
2023-05-07 22:46:13 -0600

    Se crea Formulario

--- crm-react/src/pages/NuevoCliente.jsx
+++ crm-react/src/pages/NuevoCliente.jsx
@@ -1,4 +1,9 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Form } from "react-router-dom"
+import Formulario from "../components/Formulario"
+
+export async function action({request}) {
+    const formData = await request.formData()
+}
 
 function NuevoCliente() {
     const navigate = useNavigate()
@@ -12,13 +17,27 @@ function NuevoCliente() {
                 <button
                     className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
                     type="button"
-                    onClick= {()=> navigate('/')}
+                    onClick={() => navigate(-1)}
                 >Volver</button>
             </div>
 
-             <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-                <p>Formulario aqui</p>
-             </div>
+            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+
+                <Form
+                    method="post"
+                >
+                    <Formulario />
+
+                    <input
+                        type="submit"
+                        className="mt-5 w-full bg-blue-800 uppercase font-bold text-white text-lg"
+                        value='Registrar Cliente'
+
+                    />
+
+                </Form>
+
+            </div>
         </>
     )
 }
